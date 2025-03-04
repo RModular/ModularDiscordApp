a4e2b2a0ef8733606ff10eb56d8568c1c21711d9
Sébastien GARCIA <sebastien.works@outlook.com>
2025-03-04 19:51:54 +0100

    Create update.js

---
+++ Source/Commands/update.js
@@ -0,0 +1,10 @@
+module.exports = {
+	type: "messageCreate",
+	name: "eval",
+	aliases: ["ev", "e"],
+	code: `
+$onlyIf[$authorID==876801702051856424;]
+$updateCommands
+$updateApplicationCommands
+`
+}
