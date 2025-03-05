ed5bd1ecf3b8608737d9a2cb0f244241dc814dba
Sébastien GARCIA <sebastien.works@outlook.com>
2025-03-05 23:34:14 +0100

    Update ping.js

--- Source/Applications/ping.js
+++ Source/Applications/ping.js
@@ -1,16 +1,18 @@
+// Loading the config variables.
 const config = require("../config.js");
 const main_color = `${config.COLORS.main}`
 
+// Exporting the command.
 module.exports = {
 	data: {
-		name: 'ping',
+		name: 'latency',
 		description: 'Checks bot latency.',
 		type: 1
 	},
 	code: `
 $ephemeral
-$description[**WebSocket**: $pingms
-**Round-trip**: $round[$math[$executionTime*100]]]
-$color[#FEFEFE]
+$description[WebSocket: $pingms
+Round-trip: $round[$math[$executionTime*100]]ms]
+$color[${main_color}]
 `
 }
